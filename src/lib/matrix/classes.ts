0d15655607519830619cf47104a587bbd50533e7
Holeyfield33-art <holeyfield33@gmail.com>
2026-09-12 09:23:12 -0700

    Update classes.ts index for 8 parts (AX-01..40)

--- src/lib/matrix/classes.ts
+++ src/lib/matrix/classes.ts
@@ -3,10 +3,18 @@ import { ATTACK_CLASSES_PART_1 } from "./classes-part-1";
 import { ATTACK_CLASSES_PART_2 } from "./classes-part-2";
 import { ATTACK_CLASSES_PART_3 } from "./classes-part-3";
 import { ATTACK_CLASSES_PART_4 } from "./classes-part-4";
+import { ATTACK_CLASSES_PART_5 } from "./classes-part-5";
+import { ATTACK_CLASSES_PART_6 } from "./classes-part-6";
+import { ATTACK_CLASSES_PART_7 } from "./classes-part-7";
+import { ATTACK_CLASSES_PART_8 } from "./classes-part-8";
 
 export const ATTACK_CLASSES: AttackClass[] = [
   ...ATTACK_CLASSES_PART_1,
   ...ATTACK_CLASSES_PART_2,
   ...ATTACK_CLASSES_PART_3,
   ...ATTACK_CLASSES_PART_4,
+  ...ATTACK_CLASSES_PART_5,
+  ...ATTACK_CLASSES_PART_6,
+  ...ATTACK_CLASSES_PART_7,
+  ...ATTACK_CLASSES_PART_8,
 ];
