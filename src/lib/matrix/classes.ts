fa227f8e89eff1c302696312f7ea2246c794900e
Holeyfield33-art <holeyfield33@gmail.com>
2026-09-12 09:23:48 -0700

    classes.ts index: 14 parts covering AX-01..40

--- src/lib/matrix/classes.ts
+++ src/lib/matrix/classes.ts
@@ -7,6 +7,12 @@ import { ATTACK_CLASSES_PART_5 } from "./classes-part-5";
 import { ATTACK_CLASSES_PART_6 } from "./classes-part-6";
 import { ATTACK_CLASSES_PART_7 } from "./classes-part-7";
 import { ATTACK_CLASSES_PART_8 } from "./classes-part-8";
+import { ATTACK_CLASSES_PART_9 } from "./classes-part-9";
+import { ATTACK_CLASSES_PART_10 } from "./classes-part-10";
+import { ATTACK_CLASSES_PART_11 } from "./classes-part-11";
+import { ATTACK_CLASSES_PART_12 } from "./classes-part-12";
+import { ATTACK_CLASSES_PART_13 } from "./classes-part-13";
+import { ATTACK_CLASSES_PART_14 } from "./classes-part-14";
 
 export const ATTACK_CLASSES: AttackClass[] = [
   ...ATTACK_CLASSES_PART_1,
@@ -17,4 +23,10 @@ export const ATTACK_CLASSES: AttackClass[] = [
   ...ATTACK_CLASSES_PART_6,
   ...ATTACK_CLASSES_PART_7,
   ...ATTACK_CLASSES_PART_8,
+  ...ATTACK_CLASSES_PART_9,
+  ...ATTACK_CLASSES_PART_10,
+  ...ATTACK_CLASSES_PART_11,
+  ...ATTACK_CLASSES_PART_12,
+  ...ATTACK_CLASSES_PART_13,
+  ...ATTACK_CLASSES_PART_14,
 ];
